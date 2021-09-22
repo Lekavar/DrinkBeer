0a82587817348c9083c7163055fb95257d7d88bf
Lekavar <89635694+Lekavar@users.noreply.github.com>
2021-09-22 16:28:15 +0800

    Pumpkin Kvass

--- src/main/java/lekavar/lma/drinkbeer/screen/BeerBarrelScreenHandler.java
+++ src/main/java/lekavar/lma/drinkbeer/screen/BeerBarrelScreenHandler.java
@@ -338,6 +338,8 @@ public class BeerBarrelScreenHandler extends ScreenHandler {
         map.put(Items.APPLE, "apple");
         map.put(Items.SWEET_BERRIES, "sweet_berrires");
         map.put(Items.BLUE_ICE, "blue_ice");
+        map.put(Items.BREAD, "bread");
+        map.put(Items.PUMPKIN, "pumpkin");
         return map;
     }
 
@@ -349,6 +351,7 @@ public class BeerBarrelScreenHandler extends ScreenHandler {
         map.put(DrinkBeer.BEER_MUG_APPLE_LAMBIC.asItem(), 24000);
         map.put(DrinkBeer.BEER_MUG_SWEET_BERRY_KRIEK.asItem(), 24000);
         map.put(DrinkBeer.BEER_MUG_HAARS_ICEY_PALE_LAGER.asItem(), 24000);
+        map.put(DrinkBeer.BEER_MUG_PUMPKIN_KVASS.asItem(), 12000);
         return map;
     }
 
@@ -360,6 +363,7 @@ public class BeerBarrelScreenHandler extends ScreenHandler {
         map.put(4, DrinkBeer.BEER_MUG_APPLE_LAMBIC.asItem());
         map.put(5, DrinkBeer.BEER_MUG_SWEET_BERRY_KRIEK.asItem());
         map.put(6, DrinkBeer.BEER_MUG_HAARS_ICEY_PALE_LAGER.asItem());
+        map.put(7, DrinkBeer.BEER_MUG_PUMPKIN_KVASS.asItem());
         return map;
     }
 
@@ -383,7 +387,12 @@ public class BeerBarrelScreenHandler extends ScreenHandler {
                         }
                     }
                 }
-
+                else if(map.containsKey(Items.BREAD)){
+                    if (map.get(Items.BREAD) == 2) {
+                        if (map.containsKey(Items.PUMPKIN))
+                            return new ItemStack(DrinkBeer.BEER_MUG_PUMPKIN_KVASS.asItem(), 4);
+                    }
+                }
             }
         } else if (map.containsKey(Items.WHEAT)) {
             if (map.get(Items.WHEAT) == 3) {
