262cd9d6219f01ea559007915ca6a32dfd71b948
Lekavar <89635694+Lekavar@users.noreply.github.com>
2021-10-15 16:28:49 +0800

    Place distance
    Max distance a beer block can be placed

--- src/main/java/lekavar/lma/drinkbeer/item/BeerMugBlockItem.java
+++ src/main/java/lekavar/lma/drinkbeer/item/BeerMugBlockItem.java
@@ -2,38 +2,41 @@ package lekavar.lma.drinkbeer.item;
 
 import lekavar.lma.drinkbeer.DrinkBeer;
 import net.minecraft.block.Block;
+import net.minecraft.block.BlockState;
 import net.minecraft.client.item.TooltipContext;
 import net.minecraft.entity.LivingEntity;
 import net.minecraft.entity.player.PlayerEntity;
-import net.minecraft.item.BlockItem;
-import net.minecraft.item.Item;
-import net.minecraft.item.ItemStack;
-import net.minecraft.item.Items;
+import net.minecraft.item.*;
 import net.minecraft.sound.SoundEvent;
 import net.minecraft.text.Text;
 import net.minecraft.text.TranslatableText;
 import net.minecraft.util.Formatting;
+import net.minecraft.util.math.Vec3d;
 import net.minecraft.world.World;
 import org.jetbrains.annotations.Nullable;
 
 import java.util.List;
 
-public class BeerMugBlockItem extends BlockItem {
-        public BeerMugBlockItem(Block block,Item.Settings settings) {
-            super(block,settings);
-        }
+import static java.lang.Math.pow;
+import static java.lang.Math.sqrt;
 
-        public ItemStack finishUsing(ItemStack stack, World world, LivingEntity user) {
-            ItemStack itemStack = super.finishUsing(stack, world, user);
-            if(user instanceof PlayerEntity && ((PlayerEntity)user).isCreative()){
-                return itemStack;
-            }
-            else{
-                ItemStack emptyMugItemStack = new ItemStack(DrinkBeer.EMPTY_BEER_MUG.asItem(),1);
-                ((PlayerEntity)user).giveItemStack(emptyMugItemStack);
-                return itemStack;
-            }
+public class BeerMugBlockItem extends BlockItem {
+    private final static float MAX_PLACE_DISTANCE = (float) 2;
+
+    public BeerMugBlockItem(Block block, Item.Settings settings) {
+        super(block, settings);
+    }
+
+    public ItemStack finishUsing(ItemStack stack, World world, LivingEntity user) {
+        ItemStack itemStack = super.finishUsing(stack, world, user);
+        if (user instanceof PlayerEntity && ((PlayerEntity) user).isCreative()) {
+            return itemStack;
+        } else {
+            ItemStack emptyMugItemStack = new ItemStack(DrinkBeer.EMPTY_BEER_MUG.asItem(), 1);
+            ((PlayerEntity) user).giveItemStack(emptyMugItemStack);
+            return itemStack;
         }
+    }
 
     @Override
     public SoundEvent getEatSound() {
@@ -43,10 +46,23 @@ public class BeerMugBlockItem extends BlockItem {
     @Override
     public void appendTooltip(ItemStack stack, @Nullable World world, List<Text> tooltip, TooltipContext context) {
         String name = this.asItem().toString();
-        if(this.asItem()!= DrinkBeer.BEER_MUG_PUMPKIN_KVASS.asItem()) {
+        if (this.asItem() != DrinkBeer.BEER_MUG_PUMPKIN_KVASS.asItem()) {
             tooltip.add(new TranslatableText("item.drinkbeer." + name + ".tooltip").formatted(Formatting.BLUE));
         }
         Text hunger = Text.of(String.valueOf(asItem().getFoodComponent().getHunger()));
-        tooltip.add( new TranslatableText("drinkbeer.restores_hunger").formatted(Formatting.BLUE).append(hunger));
+        tooltip.add(new TranslatableText("drinkbeer.restores_hunger").formatted(Formatting.BLUE).append(hunger));
+    }
+
+    @Override
+    protected boolean canPlace(ItemPlacementContext context, BlockState state) {
+        if (getDistance(context.getHitPos(), context.getPlayer().getPos()) > MAX_PLACE_DISTANCE)
+            return false;
+        else {
+            return super.canPlace(context, state);
+        }
+    }
+
+    private float getDistance(Vec3d p1, Vec3d p2) {
+        return (float)sqrt((pow(p1.x - p2.x, 2) + pow(p1.y - p2.y, 2) + pow(p1.z - p2.z, 2)));
     }
 }
