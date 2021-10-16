291537a965b46d34e3857d765c113608dbbb2439
Lekavar <89635694+Lekavar@users.noreply.github.com>
2021-10-16 16:37:03 +0800

    Brewing menu refine
    0:0 is removed

--- src/main/java/lekavar/lma/drinkbeer/screen/BeerBarrelScreen.java
+++ src/main/java/lekavar/lma/drinkbeer/screen/BeerBarrelScreen.java
@@ -46,7 +46,7 @@ public class BeerBarrelScreen extends HandledScreen<ScreenHandler> {
             int m = (int) (time / 60);
             int s = (int) (time % 60);
             result = m + ":" + s;
-        } else result = "0:0";
+        } else result = "";
         return result;
     }
 
